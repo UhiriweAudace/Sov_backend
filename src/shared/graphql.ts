319d8ac6b6f767125b3c5ef277302301a90e57a2
UhiriweAudace <u.audace@gmail.com>
2022-08-08 22:44:46 +0200

    removed env file

--- src/shared/graphql.ts
+++ src/shared/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,34 +9,30 @@
 /* eslint-disable */
 
 export class UserFilter {
-  page: number;
+    page: number;
 }
 
 export class User {
-  name: string;
-  height?: Nullable<string>;
-  mass?: Nullable<string>;
-  gender?: Nullable<string>;
-  homeworld?: Nullable<string>;
+    name: string;
+    height?: Nullable<string>;
+    mass?: Nullable<string>;
+    gender?: Nullable<string>;
+    homeworld?: Nullable<string>;
 }
 
 export class PageInfo {
-  totalCount: number;
+    totalCount: number;
 }
 
 export class UserResponse {
-  page_info?: Nullable<PageInfo>;
-  edges: Nullable<User>[];
+    page_info?: Nullable<PageInfo>;
+    edges: Nullable<User>[];
 }
 
 export abstract class IQuery {
-  abstract users(
-    where?: Nullable<UserFilter>,
-  ): Nullable<UserResponse> | Promise<Nullable<UserResponse>>;
+    abstract users(where?: Nullable<UserFilter>): Nullable<UserResponse> | Promise<Nullable<UserResponse>>;
 
-  abstract search(
-    keyword: string,
-  ): Nullable<UserResponse> | Promise<Nullable<UserResponse>>;
+    abstract search(keyword: string): Nullable<UserResponse> | Promise<Nullable<UserResponse>>;
 }
 
 type Nullable<T> = T | null;
